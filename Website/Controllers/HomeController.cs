602ac8814d317db4e43041cf845d26d84f5b6eb1
nguyễn thuần <vanthuan30102k@gmail.com>
2024-07-23 22:18:14 +0700

    Ui by api

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -8,14 +8,18 @@ namespace Website.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        protected IBase _ibase;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBase ibase)
         {
             _logger = logger;
+            _ibase = ibase;
         }
 
         public IActionResult Index()
         {
+            int total = 0;
+            ViewBag.listStory = _ibase.storyRespository.GetAll(0, int.MaxValue, null , null, null, ref total);
             return View();
         }
 
--- Website/Controllers/StoryController.cs
+++ Website/Controllers/StoryController.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StoryManagement.Model;
+using StoryManagement.Model.Entity;
 
 namespace Website.Controllers
 {
     public class StoryController : Controller
     {
+        protected IBase _ibase;
+        public StoryController(IBase ibase)
+        {
+            _ibase = ibase;
+        }
         public IActionResult Index()
         {
             return View();
         }
         //[Route("truyen/{slug}")]
-        public IActionResult Details(string slug)
+        public IActionResult Details(int idStory)
         {
+            Story str = _ibase.storyRespository.GetDetail(idStory);
+            ViewBag.listTags = _ibase.tagRespository.GetStoryTag(idStory);
+            ViewBag.listAuthors = _ibase.authorRespository.GetStoryAuthor(idStory);
             //int id = GetStoryIdBySlug(slug);
             //if (id == 0)
             //{
@@ -19,11 +29,13 @@ namespace Website.Controllers
 
             //var story = GetStoryById(id);
             //return View("Details", story);
-            return View();
+            return View(str);
         }
-        public IActionResult Chapter()
+        public IActionResult Chapter(int idSory)
         {
-            return View();
+            int total = 0;
+            Chapters chapters = _ibase.chapterRespository.GetAll(0, 1, idSory, ref total).FirstOrDefault();
+            return View(chapters);
         }
     }
 }
