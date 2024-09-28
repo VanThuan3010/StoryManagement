3e54f668c2889759909aff28b754d34a5b3c0ad2
VanThuan3010 <88707422+VanThuan3010@users.noreply.github.com>
2024-09-28 11:28:25 +0700

    add fuc raw vietnamese for CKEditor

--- Admin/wwwroot/js/site.js
+++ Admin/wwwroot/js/site.js
@@ -64,6 +64,49 @@ notification: function (type, message) {
         });
     }
 },
+convertToHTML: function (content) {
+    const specialChars = {
+        'à': '&agrave;',
+        'á': '&aacute;',
+        'ã': '&atilde;',
+        'â': '&acirc;',
+        'è': '&egrave;',
+        'é': '&eacute;',
+        'ê': '&ecirc;',
+        'ì': '&igrave;',
+        'í': '&iacute;',
+        'ò': '&ograve;',
+        'ó': '&oacute;',
+        'õ': '&otilde;',
+        'ô': '&ocirc;',
+        'ù': '&ugrave;',
+        'ú': '&uacute;',
+        'À': '&Agrave;',
+        'Á': '&Aacute;',
+        'Ã': '&Atilde;',
+        'Â': '&Acirc;',   
+        'È': '&Egrave;',
+        'É': '&Eacute;',
+        'Ê': '&Ecirc;',
+        'Ì': '&Igrave;',
+        'Í': '&Iacute;',
+        'Ò': '&Ograve;',
+        'Ó': '&Oacute;',
+        'Õ': '&Otilde;',
+        'Ô': '&Ocirc;',
+        'Ù': '&Ugrave;',
+        'Ú': '&Uacute;',
+        'ý': '&yacute;',
+        'Ý': '&yacute;',
+    };
+    // Đảo ngược đối tượng để thay thế từ mã HTML về ký tự
+    const invertedChars = Object.fromEntries(
+        Object.entries(specialChars).map(([key, value]) => [value, key])
+    );
+    return input.replace(/&agrave;|&aacute;|&atilde;|&acirc;|&egrave;|&eacute;|&ecirc;|&igrave;|&iacute;|&ograve;|&oacute;|&otilde;|&ocirc;|&ugrave;|&uacute;|&Agrave;|&Aacute;|&Atilde;|&Acirc;|&Egrave;|&Eacute;|&Ecirc;|&Igrave;|&Iacute;|&Ograve;|&Oacute;|&Otilde;|&Ocirc;|&Ugrave;|&Uacute;|&yacute;|&Yacute;/g, (match) => {
+        return invertedChars[match] || match;
+    });
+},
 loadButton: function (name) {
     var loading = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="ms-2">' + name + '...</span>';
     return loading;
